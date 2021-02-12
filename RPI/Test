79eb04ae7e956f2340d91a1e3656b58ddf8ec7f9
pavhr <75092379+pavhr@users.noreply.github.com>
2021-02-12 13:28:35 +0100

    V3
    V3

---
+++ RPI/Test
@@ -0,0 +1,16 @@
+from guizero import App, PushButton
+
+app = App(layout="grid")
+
+button1 = PushButton(app, text="1", grid=[0,0])
+button2 = PushButton(app, text="2", grid=[1,0])
+button3  = PushButton(app, text="3", grid=[2,0])
+button4  = PushButton(app, text="4", grid=[0,1])
+button5  = PushButton(app, text="5", grid=[1,1])
+button6  = PushButton(app, text="6", grid=[2,1])
+button7  = PushButton(app, text="7", grid=[0,2])
+button8  = PushButton(app, text="8", grid=[1,2])
+button9  = PushButton(app, text="9", grid=[2,2])
+button0  = PushButton(app, text="0", grid=[1,3])
+
+app.display()
--- RPI/main.page.py
+++ RPI/main.page.py
@@ -10,16 +10,7 @@ def Vyskladnit():
 def Naskladnit():
 	print("Požadavek pro Naskladnění")
 def service():
-	print("Potřebuju opravit")
-
-
-#hlavni_stranka
-
-
-
-
-
-
+	print("Servisní menu")
 
 
 
@@ -29,20 +20,18 @@ image1 = Picture(zahlaví, image = "logo_skoly.png", align = "left")
 image2 = Picture(zahlaví, image = "logo-text-černá.png", align = "right")
 
 hlavni_text = Text(app, "Automatický sklad", align = "top")
-hlavni_text.text_size = 50
+hlavni_text.text_size = 55
 hlavni_text.font = "Calibry"
 
 zapati = Box(app, width = "fill", align = "bottom")
-button3 = PushButton(zapati, image = "service.png", align = "left")
-#zapati.when_mouse_dragged = print("Jsi kokot!!!")
-
+button3 = PushButton(zapati, service,image = "service.png", align = "left")
 
 
 tlacitovy_box = Box(app, width = "fill", align = "bottom")
 button1 = PushButton (tlacitovy_box, Vyskladnit, text="Vyskladnit", align = "left")
 button1.text_size = 50
 button1.font = "Calibry"
-button2 = PushButton (tlacitovy_box, Naskladnit, text="Naskladnit", align = "right")
+button2 = PushButton (tlacitovy_box, Naskladnit, text="Naskladnit", align = "right", size = 20)
 button2.text_size = 50
 button2.font = "Calibry"
 
